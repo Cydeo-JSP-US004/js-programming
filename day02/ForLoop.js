bf4e31c51c50b16142579df4c275e6763acfca30
Muhtar <Muhtar.Mahmut0519@gmail.com>
2024-11-17 10:41:27 -0500

    Day02

---
+++ day02/ForLoop.js
@@ -0,0 +1,29 @@
+for (let i = 0; i < 5; i++) {
+  console.log("Hello");
+}
+
+console.log("----------------------------------------------------------------");
+
+for (let index = 0; index < 10; index++) {
+  console.log(index);
+}
+
+console.log("----------------------------------------------------------------");
+
+for (let i = 1; i < 8; i++) {
+  console.log(i); // 1 2 3 4
+  if (i == 4) {
+    break; // terminates the loop
+  }
+}
+
+console.log("----------------------------------------------------------------");
+
+for (let i = 1; i < 8; i++) {
+    if(i == 2 || i == 6){
+        continue; // jumps to next iteration (skips the current iteration)
+    }
+    console.log(i);
+}
+
+
